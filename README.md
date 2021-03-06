93026181245b266f28f126e9590bea322b7d1640
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2021-03-06 13:30:33 +0100

    Adicionar Menus

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # crypto-tracker-desktop-app
 Electronjs Crypto Tracker
+
+
+
+# Installation
+´´´ 
+  npm install electron --save-dev --save-exact
+  
+´´´
---
+++ main.js
@@ -0,0 +1,56 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const shell = require('electron').shell;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('src/index.html')
+
+    var menu = Menu.buildFromTemplate([{
+            label: 'Menu',
+            submenu: [
+                { label: 'Adjust Notification Value' },
+                {
+                    label: 'CoinMarketCap',
+                    click() {
+                        shell.openExternal('http://coinmarketcap.com')
+                    }
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit();
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Info'
+        }
+    ]);
+
+    Menu.setApplicationMenu(menu)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
